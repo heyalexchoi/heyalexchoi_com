6b7067a0fa1d447f286e9873d381063af70cbd68
alex choi <heyalexchoi@gmail.com>
2025-11-24 18:37:10 -0500

    add _meta, favicon, globals.css, and update exo post

---
+++ app/_meta.global.js
@@ -0,0 +1,10 @@
+export default {
+  index: {
+    type: 'page',
+    title: 'About',
+  },
+  exo: {
+    type: 'page',
+    title: 'Exo',
+  }
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Footer, Layout, Navbar, ThemeSwitch } from 'nextra-theme-blog'
 import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-blog/style.css'
+import './globals.css'
 
 export const metadata = {
   title: 'Hey Alex Choi',
@@ -13,15 +14,13 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head  />
+      <Head />
       <body>
         <Layout>
           <Navbar pageMap={pageMap}>
             <ThemeSwitch />
           </Navbar>
-
           {children}
-
           <Footer>
             © {new Date().getFullYear()} Alex Choi.
           </Footer>
