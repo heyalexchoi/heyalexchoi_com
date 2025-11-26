b6223ee30a9c67aa66a4afd58784af95a066a2d4
alex choi <heyalexchoi@gmail.com>
2025-11-25 19:07:15 -0500

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,9 @@ const withNextra = nextra({})
 
 export default withNextra({
   output: 'export',
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+  },
   trailingSlash: true, // Optional but often helps with routing (claude says)
 })
 
